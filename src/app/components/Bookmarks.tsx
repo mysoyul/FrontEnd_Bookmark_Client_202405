a260986daef99a0d029ed67439c8465f712f677d
mysoyul <vega2k_@naver.com>
2024-05-21 15:15:43 +0900

    Bookmarks 대신 BookmarksResponse 사용

--- src/app/components/Bookmarks.tsx
+++ src/app/components/Bookmarks.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { BookmarkType } from '@/types';
+import { BookmarksResponse } from '@/types';
 import Bookmark from '@/app/components/Bookmark';
 
 type Props = {
-    bookmarks: BookmarkType[]
+    bookmarks: BookmarksResponse
 }
 
 const Bookmarks: React.FC<Props> = ({bookmarks}) => {
@@ -11,7 +11,7 @@ const Bookmarks: React.FC<Props> = ({bookmarks}) => {
         <div>
             <h1>Welcome to Bookmarker</h1>
             {
-            bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark} />)
+            bookmarks.data.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark} />)
             }            
         </div>
     );
